649ac69a15ee97b420a0a81acd3a3f5709e94f99
baba-beda <darya.nightmare@gmail.com>
2015-06-23 19:25:37 +0300

    now it's working

--- foreach/foreach.c
+++ foreach/foreach.c
@@ -19,18 +19,16 @@ void write_char(char c)
         error_foreach(strerror(errno));
     }
 }
-void execute(char* file_path, char* argv[]) {
+void execute(char* file_path, char* argv[], int argc) {
     int res = spawn(file_path, argv);
     if (res == 0) {
         int write_cnt;
-        write_cnt = write_(STDOUT_FILENO, file_path, strlen(file_path));
-        if (write_cnt == -1) {
-            error_foreach(strerror(errno));
-        }
-        write_char(' ');
-        write_cnt = write_(STDOUT_FILENO, argv[1], strlen(argv[1]));
-        if (write_cnt == -1) {
-            error_foreach(strerror(errno));
+        for (int i = 0; i < argc; i++) {
+            write_cnt = write_(STDOUT_FILENO, argv[i], strlen(argv[i]));
+            if (write_cnt == -1) {
+                error_foreach(strerror(errno));
+            }
+            write_char(' ');
         }
         write_char('\n');
     }
@@ -54,10 +52,13 @@ int main(int argc, char ** argv) {
 
         for (int i = 0; i < read_cnt; i++) {
             if (command[i] == ' ') {
-                counter++;
+                args[counter++][arg_len] = '\0';
                 args[counter] = malloc(4096);
                 arg_len = 0;
             } else {
+                if (command[i] == '\n') {
+                    args[counter][arg_len++] = '\0';
+                }
                 args[counter][arg_len++] = command[i];
             }
         }
@@ -65,11 +66,13 @@ int main(int argc, char ** argv) {
         command = malloc(4096);
     } while (read_cnt > 0);
 
-
+    if (arg_len != 0) {
+        counter++;
+    }
 
     for (int i = 1; i < argc; i++) {
         args[counter] = argv[i];
         args[counter + 1] = NULL;
-        execute(args[0], args);
+        execute(args[0], args, counter + 1);
     }
 }
